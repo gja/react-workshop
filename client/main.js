a03963005b08edf63992952d55944fa1cc449051
Tejas Dinkar <tejas@gja.in>
2015-09-11 13:21:54 +0530

    Browserify and react working

--- client/main.js
+++ client/main.js
@@ -1,1 +1,11 @@
-alert("hi");
+var React = require("react");
+
+var ChatClient = React.createClass({
+  render: function() {
+    return React.createElement("div", {}, "This is awesome!");
+  }
+});
+
+global.startChatClient = function(container) {
+  React.render(React.createElement(ChatClient), container);
+};
